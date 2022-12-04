247575ee72a4b763bc0a78fa8d1fcd5025cb7932
ariopri <arioprima1@gmail.com>
2022-12-04 20:17:51 +0700

    feat: create Hero components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
+import Hero from './components/hero/Hero';
 import NavigationBar from './components/navigationBar/NavigationBar';
 
 function App() {
-  return <NavigationBar />;
+  return (
+    <>
+      <NavigationBar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
