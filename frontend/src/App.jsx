1ce767d251f30fc6d6e5d25f597c0c66e1c9f947
ariopri <arioprima1@gmail.com>
2022-12-11 17:11:40 +0700

    feat: create about us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
 import HomePage from './pages/HomePage';
 import RoutesOutlet from './pages/RoutesOutlet';
 import { Routes, Route } from 'react-router-dom';
-import DaftarMentor from './components/daftarMentor/DaftarMentor';
-import Testimoni from './components/testimoni/Testimoni';
-import Faq from './components/faq/Faq';
+import Informasi from './pages/informasi/Informasi';
+import About from './pages/About';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<RoutesOutlet />}>
         <Route index element={<HomePage />} />
-        <Route index element={<DaftarMentor />} />
-        <Route index element={<Testimoni />} />
-        <Route index element={<Faq />} />
+        <Route path="informasi" element={<Informasi />} />
+        <Route path="tentang" element={<About />} />
       </Route>
     </Routes>
   );
