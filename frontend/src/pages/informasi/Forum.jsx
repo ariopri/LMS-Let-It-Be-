51dbc59c4cd9d18db6e8484dc2837648641b990d
ariopri <arioprima1@gmail.com>
2022-12-11 14:31:20 +0700

    create a forum component for information pages

---
+++ frontend/src/pages/informasi/Forum.jsx
@@ -0,0 +1,11 @@
+import Brainly from '../../assets/brainly.png';
+
+const Forum = [
+  {
+    name: 'brainly',
+    url: 'https://brainly.co.id/',
+    image: Brainly,
+  },
+];
+
+export default Forum;
