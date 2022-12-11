9b81b6dba3807b034343c52a51806de004d1cc63
ariopri <arioprima1@gmail.com>
2022-12-11 14:33:11 +0700

    feat: create information page

---
+++ frontend/src/pages/informasi/Informasi.jsx
@@ -0,0 +1,83 @@
+import {
+  Container,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import LoadingFetchEffect from '../../components/loadingEffect/LoadingFetchEffect';
+import useTimeoutGlobal from '../../store/TimeOut';
+import Belajar from './Belajar';
+import Bootcamp from './Bootcamp';
+import Forum from './Forum';
+import TryOut from './TryOut';
+
+export default function Informasi() {
+  const { timeout } = useTimeoutGlobal();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, timeout);
+  });
+
+  const textcolor = useColorModeValue('accentLight.500', 'accentDark.500');
+
+  const ContainerInformasi = props => {
+    return (
+      <Stack mt={5}>
+        <Text fontWeight={'semibold'}>{props.judul}</Text>
+        <SimpleGrid
+          columns={{ base: 3, sm: 4, md: 5, lg: 8 }}
+          autoRows="1fr"
+          gap={2}>
+          {props.data.map((item, index) => (
+            <Stack
+              as="a"
+              href={item.url}
+              target="_blank"
+              borderWidth={1}
+              w="full"
+              h="60px"
+              p={2}
+              bg="white"
+              key={index}>
+              <Image
+                src={item.image}
+                alt={item.name}
+                fit={'contain'}
+                w="full"
+                h="full"
+              />
+            </Stack>
+          ))}
+        </SimpleGrid>
+      </Stack>
+    );
+  };
+
+  return isLoading ? (
+    <LoadingFetchEffect />
+  ) : (
+    <Container maxW="7xl" data-aos="fade-up" pt={4}>
+      <Heading
+        fontSize={{ base: '2xl', sm: '4xl', md: '5xl' }}
+        color={textcolor}>
+        Informasi
+      </Heading>
+      <Text color={'gray.500'}>
+        korem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        condimentum, nisl eget ultricies tincidunt, nisl elit lacinia nunc, ut
+        aliquam massa nisl eget dolor. Sed euismod, nisl eget ultricies
+        tincidunt, nisl elit lacinia nunc, ut aliquam massa nisl eget dolor.
+      </Text>
+      <ContainerInformasi judul="Belajar" data={Belajar} />
+      <ContainerInformasi judul="Forum" data={Forum} />
+      <ContainerInformasi judul="TryOut" data={TryOut} />
+      <ContainerInformasi judul="Bootcamp" data={Bootcamp} />
+    </Container>
+  );
+}
