92df01ea7a14a63758acd10bdb58f3733825084a
ariopri <arioprima1@gmail.com>
2022-12-11 14:31:39 +0700

    feat: create a bootcamp component for information pages

---
+++ frontend/src/pages/informasi/Bootcamp.jsx
@@ -0,0 +1,11 @@
+import AlumniUi from '../../assets/bimbingan.png';
+
+const Bootcamp = [
+  {
+    name: 'alumni-ui',
+    url: 'https://alumni-ui.web.app/',
+    image: AlumniUi,
+  },
+];
+
+export default Bootcamp;
