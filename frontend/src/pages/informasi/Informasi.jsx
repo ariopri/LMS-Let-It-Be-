08e6289b7d44745d01820c485a53c421ff6b1fb8
ariopri <arioprima1@gmail.com>
2022-12-20 00:00:25 +0700

    chore(fe): improve the programming logic to make the source code more structured

--- frontend/src/pages/informasi/Informasi.jsx
+++ frontend/src/pages/informasi/Informasi.jsx
@@ -12,61 +12,55 @@ import LoadingFetchEffect from '../../components/loadingEffect/LoadingFetchEffec
 import axios from 'axios';
 
 export default function Informasi() {
-  const [fetchInformasi, setFetchInformasi] = useState([]);
+  const [informasi, setInformasi] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = () => {
+  useEffect(() => {
     axios
       .get('https://639ed8157aaf11ceb88c33f5.mockapi.io/ngasal3')
       .then(res => {
-        setFetchInformasi(res.data);
+        setInformasi(res.data);
         setIsLoading(false);
       })
       .catch(err => {
         console.log(err);
       });
-  };
-
-  useEffect(() => {
-    fetchData();
   }, []);
 
   const textcolor = useColorModeValue('accentLight.500', 'accentDark.500');
 
-  const ContainerInformasi = props => {
-    const { judul } = props;
-    const filteredData = fetchInformasi.filter(item => item.judul === judul);
+  const ContainerInformasi = ({ judul }) => {
+    const filteredData = informasi.filter(item => item.judul === judul);
+    const data = filteredData[0]?.data || [];
 
     return (
       <Stack mt={5}>
         <Text fontWeight={'semibold'}>{judul}</Text>
-        {filteredData.map(({ id, data }) => (
-          <SimpleGrid
-            key={id}
-            columns={{ base: 3, sm: 4, md: 5, lg: 8 }}
-            autoRows="1fr"
-            gap={2}>
-            {data.map(({ url, image, judul }) => (
-              <Stack
-                as="a"
-                href={url}
-                target="_blank"
-                borderWidth={1}
+        <SimpleGrid
+          columns={{ base: 3, sm: 4, md: 5, lg: 8 }}
+          autoRows="1fr"
+          gap={2}>
+          {data.map(({ url, image, judul }, index) => (
+            <Stack
+              as="a"
+              href={url}
+              target="_blank"
+              borderWidth={1}
+              w="full"
+              h="60px"
+              p={2}
+              bg="white"
+              key={index}>
+              <Image
+                src={image}
+                alt={judul}
+                fit={'contain'}
                 w="full"
-                h="60px"
-                p={2}
-                bg="white">
-                <Image
-                  src={image}
-                  alt={judul}
-                  fit={'contain'}
-                  w="full"
-                  h="full"
-                />
-              </Stack>
-            ))}
-          </SimpleGrid>
-        ))}
+                h="full"
+              />
+            </Stack>
+          ))}
+        </SimpleGrid>
       </Stack>
     );
   };
@@ -80,13 +74,11 @@ export default function Informasi() {
         color={textcolor}>
         Informasi
       </Heading>
-      <Text color={'gray.500'}>
-        {fetchInformasi.map((item, index) => (
-          <span key={index}>{item.keterangan}</span>
-        ))}
-      </Text>
-      {fetchInformasi.map((item, index) => (
-        <ContainerInformasi key={index} judul={item.judul} />
+      {informasi[0] && (
+        <Text color={'gray.500'}>{informasi[0].keterangan}</Text>
+      )}
+      {informasi.map(({ judul }, index) => (
+        <ContainerInformasi key={index} judul={judul} />
       ))}
     </Container>
   );
