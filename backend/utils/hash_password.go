ed85fd4c0f9390b86fc469aabf0455e0e055dbd0
ariopri <arioprima1@gmail.com>
2023-06-18 21:33:35 +0700

    create hash password

---
+++ backend/utils/hash_password.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("error hashing password: %v", err)
+	}
+	return string(hashedPassword), nil
+}
+
+func VerifyPassword(hashedPassword string, candidatePassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(candidatePassword))
+}
