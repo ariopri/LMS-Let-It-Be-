07b788b999e1f160ce7e8d7b70eb6f115a92bccf
ariopri <arioprima1@gmail.com>
2023-06-18 21:33:52 +0700

    create helper tx

---
+++ backend/helper/tx.go
@@ -0,0 +1,14 @@
+package helper
+
+import "database/sql"
+
+func CommitOrRollback(tx *sql.Tx) {
+	err := recover()
+	if err != nil {
+		errorRollback := tx.Rollback()
+		PanicIfError(errorRollback)
+	} else {
+		errorCommit := tx.Commit()
+		PanicIfError(errorCommit)
+	}
+}
