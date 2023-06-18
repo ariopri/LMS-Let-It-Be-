94c3ce1900b63f304b917fd34c427f55fe86f4bd
ariopri <arioprima1@gmail.com>
2023-06-18 21:51:58 +0700

    menambah struct yang belum terkonfigurasi

--- backend/models/web/request/user_create_request.go
+++ backend/models/web/request/user_create_request.go
@@ -1,9 +1,12 @@
 package request
 
+import "time"
+
 type UserCreateRequest struct {
-	Firstname string `json:"firstname" validate:"required,min=1,max=20"`
-	Lastname  string `json:"lastname" validate:"required,min=1,max=20"`
-	Email     string `json:"email" validate:"required,email"`
-	Username  string `json:"username" validate:"required,min=1,max=20,lowercase"`
-	Password  string `json:"password" validate:"required,min=5,max=20"`
+	Firstname string    `json:"firstname" validate:"required,min=1,max=20"`
+	Lastname  string    `json:"lastname" validate:"required,min=1,max=20"`
+	Email     string    `json:"email" validate:"required,email"`
+	Username  string    `json:"username" validate:"required,min=1,max=20,lowercase"`
+	Password  string    `json:"password" validate:"required,min=5,max=20"`
+	CreatedAt time.Time `json:"created_at"`
 }
