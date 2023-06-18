7b066dbad4d2d06514d98a2cc966a897282beefd
ariopri <arioprima1@gmail.com>
2023-06-18 13:45:33 +0700

    mengubah tipe data created_at dan update_at

--- backend/models/domain/user.go
+++ backend/models/domain/user.go
@@ -1,13 +1,15 @@
 package domain
 
+import "time"
+
 type User struct {
-	Id        int    `json:"id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Username  string `json:"username"`
-	email     string `json:"email"`
-	Password  string `json:"password"`
-	Role      string `json:"role"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	Id        int       `json:"id"`
+	FirstName string    `json:"firstname"`
+	LastName  string    `json:"lastname"`
+	Username  string    `json:"username"`
+	email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
