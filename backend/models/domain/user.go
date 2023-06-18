4c3642efb555f70b3993572f590467eb757976eb
ariopri <arioprima1@gmail.com>
2023-06-18 20:11:30 +0700

    fix typo

--- backend/models/domain/user.go
+++ backend/models/domain/user.go
@@ -7,7 +7,7 @@ type User struct {
 	FirstName string    `json:"firstname"`
 	LastName  string    `json:"lastname"`
 	Username  string    `json:"username"`
-	email     string    `json:"email"`
+	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	Role      string    `json:"role"`
 	CreatedAt time.Time `json:"created_at"`
