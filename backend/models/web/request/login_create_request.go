2caf13f795168692bf77abab9967106ccb67f790
ariopri <arioprima1@gmail.com>
2023-06-18 21:32:44 +0700

    fix typo

--- backend/models/web/request/login_create_request.go
+++ backend/models/web/request/login_create_request.go
@@ -1,6 +1,6 @@
 package request
 
 type LoginCreateRequest struct {
-	username string `json:"username" validate:"required,min=1,max=20,lowercase"`
-	password string `json:"password" validate:"required,min=5,max=20"`
+	Username string `json:"username" validate:"required,min=1,max=20,lowercase"`
+	Password string `json:"password" validate:"required,min=5,max=20"`
 }
--- backend/models/web/response/register_response.go
+++ backend/models/web/response/register_response.go
@@ -1,7 +1,6 @@
 package response
 
 type RegisterResponse struct {
-	Message string `json:"message"`
-	Token   string `json:"token"`
-	Role    string `json:"role"`
+	Token string `json:"token"`
+	Role  string `json:"role"`
 }
