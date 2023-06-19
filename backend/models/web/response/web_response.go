5e264d404248974c8a31511fc8f10fe19721a8c7
ariopri <arioprima1@gmail.com>
2023-06-19 13:18:31 +0700

    create web response

---
+++ backend/models/web/response/web_response.go
@@ -0,0 +1,8 @@
+package response
+
+type WebResponse struct {
+	Code    int         `json:"code"`
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
