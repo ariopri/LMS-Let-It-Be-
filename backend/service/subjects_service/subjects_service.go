5f91789a64ef2d1670bf0a952c84209216509525
ariopri <arioprima1@gmail.com>
2023-06-30 23:56:45 +0700

    fix bug subject service

--- backend/service/subjects_service/subjects_service.go
+++ backend/service/subjects_service/subjects_service.go
@@ -7,10 +7,10 @@ import (
 )
 
 type SubjectsService interface {
-	Create(ctx context.Context, request request.SubjectCreateRequest) response.WebResponse
-	Update(ctx context.Context, request request.SubjectUpdateRequest) response.WebResponse
+	Create(ctx context.Context, request request.SubjectCreateRequest) response.SubjectsResponse
+	Update(ctx context.Context, request request.SubjectUpdateRequest) response.SubjectsResponse
 	Delete(ctx context.Context, subjectsId int)
-	FindById(ctx context.Context, subjectsId int) response.WebResponse
-	FindByUsername(ctx context.Context, subjectsName string) response.WebResponse
+	FindById(ctx context.Context, subjectsId int) response.SubjectsResponse
+	FindByUsername(ctx context.Context, subjectsName string) response.SubjectsResponse
 	FindAll(ctx context.Context) []response.SubjectsResponse
 }
