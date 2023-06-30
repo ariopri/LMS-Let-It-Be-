a8734860b67bdbe2eb553860c7036f2d842f5d42
ariopri <arioprima1@gmail.com>
2023-06-30 23:56:21 +0700

    create helper SubjectsResponse

--- backend/helper/model.go
+++ backend/helper/model.go
@@ -1,11 +1,21 @@
 package helper
 
-import "github.com/ariopri/MassiveProject/models/web/response"
+import (
+	"github.com/ariopri/MassiveProject/models/domain"
+	"github.com/ariopri/MassiveProject/models/web/response"
+)
 
 func ToLoginResponse(token, role string) response.LoginResponse {
 	return response.LoginResponse{
 		TokenType: "Bearer",
 		Token:     token,
 		Role:      role,
+	}
+}
+
+func ToSubjectsResponse(subjects domain.Subject) response.SubjectsResponse {
+	return response.SubjectsResponse{
+		Id:          int(subjects.Id),
+		SubjectName: subjects.SubjectName,
 	}
 }
