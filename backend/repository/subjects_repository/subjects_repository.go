92fa20685bc56acee93552797d8dc59d32b8fc5a
ariopri <arioprima1@gmail.com>
2023-06-30 23:16:49 +0700

    change subjects repository interface

---
+++ backend/repository/subjects_repository/subjects_repository.go
@@ -0,0 +1,16 @@
+package subjects_repository
+
+import (
+	"context"
+	"database/sql"
+	"github.com/ariopri/MassiveProject/models/domain"
+)
+
+type SubjectRepository interface {
+	Save(ctx context.Context, tx *sql.Tx, subject domain.Subject) domain.Subject
+	Update(ctx context.Context, tx *sql.Tx, subject domain.Subject) domain.Subject
+	Delete(ctx context.Context, tx *sql.Tx, subject domain.Subject)
+	FindByID(ctx context.Context, tx *sql.Tx, subjectID int) (domain.Subject, error)
+	FindBySubjectName(ctx context.Context, tx *sql.Tx, subjectName string) (domain.Subject, error)
+	FindAll(ctx context.Context, tx *sql.Tx) []domain.Subject
+}
