cbf4fa6d62484609d9205551db18fe785d5e3f4e
ariopri <arioprima1@gmail.com>
2023-07-07 15:41:31 +0700

    create navigation bar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/aos.d.ts
@@ -0,0 +1,13 @@
+declare module 'aos' {
+    interface AosOptions {
+      duration?: number;
+    }
+
+    interface Aos {
+      init(options?: AosOptions): void;
+    }
+
+    const AOS: Aos;
+
+    export default AOS;
+  }
